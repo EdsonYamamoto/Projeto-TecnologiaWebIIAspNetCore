e9c5da5441a36de9d8e6fc6842a3bc6f935a4801
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2018-10-27 18:24:27 -0300

    Commit inicial

---
+++ TecnologiaWebIIAspNetCore/TecnologiaWebIIAspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TecnologiaWebIIAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
